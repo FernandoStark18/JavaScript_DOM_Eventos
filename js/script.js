48adbf669d0118afcae92e39761bca12a0542579
FernandoStark18 <fernandostark18@gmail.com>
2022-07-30 17:49:14 -0600

    Seleccionar elementos con querySelectorAll

--- js/script.js
+++ js/script.js
@@ -4,9 +4,14 @@ heading.textContent = 'Nuevo Heading';
 heading.classList.add('nueva-clase');
 console.log(heading);
 
-
 // querySelectorAll
-
-
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces);
+enlaces[0].textContent = 'Nuevo texto para enlace'
+enlaces[0].href = 'http://www.google.com/';
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
 
 // getElementById
+const heading2 = document.getElementById('heading');
+console.log(heading2);
