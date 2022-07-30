fe962b457cd4e8ec8bde6a7c2a4fafda10164657
FernandoStark18 <fernandostark18@gmail.com>
2022-07-30 17:28:31 -0600

    Query selector
    Seleccionar elementos con querySelector

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// querySelector
+const heading = document.querySelector('.header__texto h2'); // Retorna 0 o 1 elemento
+heading.textContent = 'Nuevo Heading';
+heading.classList.add('nueva-clase');
+console.log(heading);
+
+
+// querySelectorAll
+
+
+
+// getElementById
