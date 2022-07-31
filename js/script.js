03a42acb83d92881884f6fd5065763c54a704173
FernandoStark18 <fernandostark18@gmail.com>
2022-07-30 18:23:14 -0600

    Creando HTML desde JavaScript

--- js/script.js
+++ js/script.js
@@ -10,8 +10,26 @@ console.log(enlaces);
 enlaces[0].textContent = 'Nuevo texto para enlace'
 enlaces[0].href = 'http://www.google.com/';
 enlaces[0].classList.add('nueva-clase');
-enlaces[0].classList.remove('navegacion__enlace');
+// enlaces[0].classList.remove('navegacion__enlace');
 
 // getElementById
 const heading2 = document.getElementById('heading');
 console.log(heading2);
+
+// Generar un nuevo enlace
+const nuevoEnlace = document.createElement('A');
+
+// Agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+
+// Agregar el texto
+nuevoEnlace.textContent = 'Tienda virtual';
+
+// Agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+// Agregarlo al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace);
